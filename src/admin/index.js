785ed6583e8b7587e105a5ce802483382eb251fc
Isaac <105789412+altaskur@users.noreply.github.com>
2024-03-30 21:58:23 +0100

    Actualizar estilos y etiquetas de HTML en la página de administración

--- src/admin/index.js
+++ src/admin/index.js
@@ -2,10 +2,22 @@ const SSE = new EventSource('http://localhost:3007/sse');
 
 const statusElement = document.querySelector('.display-status');
 
+const selectElement = document.querySelector('select');
+
+selectElement.addEventListener('change', (event) => {
+  const status = event.target.value;
+  fetch('http://localhost:3007/change', {
+    method: 'POST',
+    body: `status=${status}`,
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+    },
+  });
+});
+
 SSE.onmessage = (event) => {
   const data = JSON.parse(event.data);
   statusElement.textContent = data.status;
-  console.log('Event', data);
 };
 
 const formElement = document.querySelector('form');
@@ -14,6 +26,7 @@ formElement.addEventListener('submit', (event) => {
   event.preventDefault();
   const formData = new FormData(formElement);
   const status = formData.get('status');
+  console.log(status);
   fetch('http://localhost:3007/change', {
     method: 'POST',
     body: `status=${status}`,
