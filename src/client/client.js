eb541d78bcdd742e882cb847cfe73364adad79c1
Isaac <105789412+altaskur@users.noreply.github.com>
2024-04-22 23:24:29 +0200

    feat: :sparkles: add statics css and refactor UI of client mode

--- src/client/client.js
+++ src/client/client.js
@@ -1,22 +1,17 @@
 const serverAddress = `http://${window.location.hostname}:3007`;
-const iElement = document.querySelector('i');
 const messageElement = document.querySelector('p');
 
 try {
   const SSE = new EventSource(`${serverAddress}/sse`);
   SSE.onopen = () => {
-    iElement.textContent = 'Connected';
+    messageElement.textContent = 'Connected';
   };
+
   SSE.onmessage = (event) => {
-    iElement.textContent = console.log('Hello from client');
     const data = JSON.parse(event.data);
     messageElement.textContent = data.status;
   };
-
-  iElement.textContent = 'Connected';
 } catch (error) {
+  messageElement.textContent = `Error ${error}`;
   console.log('Error:', error);
-  iElement.textContent = error;
 }
-
-iElement.textContent = 'Connected';
--- src/router/staticRouter.js
+++ src/router/staticRouter.js
@@ -4,5 +4,6 @@ const router = express.Router();
 
 router.use(express.static('src/client'));
 router.use('/admin', express.static('src/admin'));
+router.use('/assets/', express.static('src/static'));
 
 module.exports = router;
