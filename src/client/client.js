0a91efbc2a210f13ac96f662b5c92c735b05dc52
Isaac <105789412+altaskur@users.noreply.github.com>
2024-04-22 22:24:02 +0200

    feat: :sparkles: add css for variables

--- src/client/client.js
+++ src/client/client.js
@@ -1,11 +1,22 @@
-window.onload = () => {
-  const serverAddress = `http://${window.location.hostname}:3007`;
+const serverAddress = `http://${window.location.hostname}:3007`;
+const iElement = document.querySelector('i');
+const messageElement = document.querySelector('p');
 
+try {
   const SSE = new EventSource(`${serverAddress}/sse`);
-  const messageElement = document.querySelector('p');
-
+  SSE.onopen = () => {
+    iElement.textContent = 'Connected';
+  };
   SSE.onmessage = (event) => {
+    iElement.textContent = console.log('Hello from client');
     const data = JSON.parse(event.data);
     messageElement.textContent = data.status;
   };
-};
+
+  iElement.textContent = 'Connected';
+} catch (error) {
+  console.log('Error:', error);
+  iElement.textContent = error;
+}
+
+iElement.textContent = 'Connected';
